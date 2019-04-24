df9b8fd6b86404104f88fd152f6611f26d2afbcf
Marco Signoretto <marco.signoretto.dev@gmail.com>
2019-04-24 22:40:56 +0400

    Simple listeners registration

--- app/src/main/java/it/marcosignoretto/swipeviewexample/MainActivity.kt
+++ app/src/main/java/it/marcosignoretto/swipeviewexample/MainActivity.kt
@@ -1,12 +1,34 @@
 package it.marcosignoretto.swipeviewexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import it.marcosignoretto.swipeview.SwipeView
+import it.marcosignoretto.swipeview.SwipeViewListener
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val view1 = findViewById<SwipeView>(R.id.view1)
+        view1.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "Front view clicked", Toast.LENGTH_SHORT).show()
+        })
+        view1.setOnBackViewClickListener(View.OnClickListener {
+            Toast.makeText(this, "Back view clicked", Toast.LENGTH_SHORT).show()
+        })
+        view1.setOnSwipeViewListener(object : SwipeViewListener {
+            override fun onOpen() {
+                Toast.makeText(this@MainActivity, "View opened", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onClose() {
+                Toast.makeText(this@MainActivity, "View closed", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
